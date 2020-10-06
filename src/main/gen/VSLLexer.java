f5d8af76620373bd53bccd3d821c6b4cf1cb74fd
mdugue <mathias.dugue@etudiant.univ-rennes1.fr>
2020-10-06 10:01:07 +0200

    Add Sub Rules

---
+++ src/main/gen/VSLLexer.java
@@ -0,0 +1,153 @@
+// Generated from C:/Users/Mech-Robo Neko-Stro/istic/M1/PDS/PDS-TP2-java/src/main/antlr\VSLLexer.g4 by ANTLR 4.8
+
+  package TP2;
+
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.misc.*;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class VSLLexer extends Lexer {
+	static { RuntimeMetaData.checkVersion("4.8", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		WS=1, COMMENT=2, LP=3, RP=4, PLUS=5, IDENT=6, TEXT=7, INTEGER=8;
+	public static String[] channelNames = {
+		"DEFAULT_TOKEN_CHANNEL", "HIDDEN"
+	};
+
+	public static String[] modeNames = {
+		"DEFAULT_MODE"
+	};
+
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"WS", "COMMENT", "LETTER", "DIGIT", "ASCII", "LP", "RP", "PLUS", "IDENT", 
+			"TEXT", "INTEGER"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, null, null, "'('", "')'", "'+'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, "WS", "COMMENT", "LP", "RP", "PLUS", "IDENT", "TEXT", "INTEGER"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+
+	public VSLLexer(CharStream input) {
+		super(input);
+		_interp = new LexerATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	@Override
+	public String getGrammarFileName() { return "VSLLexer.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public String[] getChannelNames() { return channelNames; }
+
+	@Override
+	public String[] getModeNames() { return modeNames; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	@Override
+	public void action(RuleContext _localctx, int ruleIndex, int actionIndex) {
+		switch (ruleIndex) {
+		case 9:
+			TEXT_action((RuleContext)_localctx, actionIndex);
+			break;
+		}
+	}
+	private void TEXT_action(RuleContext _localctx, int actionIndex) {
+		switch (actionIndex) {
+		case 0:
+			 setText(getText().substring(1, getText().length() - 1)); 
+			break;
+		}
+	}
+
+	public static final String _serializedATN =
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\nK\b\1\4\2\t\2\4"+
+		"\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t"+
+		"\13\4\f\t\f\3\2\3\2\3\2\3\2\3\3\3\3\3\3\3\3\7\3\"\n\3\f\3\16\3%\13\3\3"+
+		"\3\3\3\3\4\3\4\3\5\3\5\3\6\3\6\3\7\3\7\3\b\3\b\3\t\3\t\3\n\3\n\3\n\7\n"+
+		"8\n\n\f\n\16\n;\13\n\3\13\3\13\7\13?\n\13\f\13\16\13B\13\13\3\13\3\13"+
+		"\3\13\3\f\6\fH\n\f\r\f\16\fI\2\2\r\3\3\5\4\7\2\t\2\13\2\r\5\17\6\21\7"+
+		"\23\b\25\t\27\n\3\2\5\4\2\13\f\"\"\3\2\f\f\4\2\f\f$$\2L\2\3\3\2\2\2\2"+
+		"\5\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3\2\2\2\2\25\3\2"+
+		"\2\2\2\27\3\2\2\2\3\31\3\2\2\2\5\35\3\2\2\2\7(\3\2\2\2\t*\3\2\2\2\13,"+
+		"\3\2\2\2\r.\3\2\2\2\17\60\3\2\2\2\21\62\3\2\2\2\23\64\3\2\2\2\25<\3\2"+
+		"\2\2\27G\3\2\2\2\31\32\t\2\2\2\32\33\3\2\2\2\33\34\b\2\2\2\34\4\3\2\2"+
+		"\2\35\36\7\61\2\2\36\37\7\61\2\2\37#\3\2\2\2 \"\n\3\2\2! \3\2\2\2\"%\3"+
+		"\2\2\2#!\3\2\2\2#$\3\2\2\2$&\3\2\2\2%#\3\2\2\2&\'\b\3\2\2\'\6\3\2\2\2"+
+		"()\4c|\2)\b\3\2\2\2*+\4\62;\2+\n\3\2\2\2,-\n\4\2\2-\f\3\2\2\2./\7*\2\2"+
+		"/\16\3\2\2\2\60\61\7+\2\2\61\20\3\2\2\2\62\63\7-\2\2\63\22\3\2\2\2\64"+
+		"9\5\7\4\2\658\5\7\4\2\668\5\t\5\2\67\65\3\2\2\2\67\66\3\2\2\28;\3\2\2"+
+		"\29\67\3\2\2\29:\3\2\2\2:\24\3\2\2\2;9\3\2\2\2<@\7$\2\2=?\5\13\6\2>=\3"+
+		"\2\2\2?B\3\2\2\2@>\3\2\2\2@A\3\2\2\2AC\3\2\2\2B@\3\2\2\2CD\7$\2\2DE\b"+
+		"\13\3\2E\26\3\2\2\2FH\5\t\5\2GF\3\2\2\2HI\3\2\2\2IG\3\2\2\2IJ\3\2\2\2"+
+		"J\30\3\2\2\2\b\2#\679@I\4\b\2\2\3\13\2";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
