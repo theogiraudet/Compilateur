b6c540d748d148680982bb55895d5b7b566cc910
Giraudet Théo <theo.giraudet@etudiant.univ-rennes1.fr>
2020-11-17 09:22:37 +0100

    Revert "Fixe ordonance ir while"
    This reverts commit 3c043e7e55d30b1b889c8bab6e216e4b9843eb00

--- src/main/java/TP2/asd/statement/Statement.java
+++ src/main/java/TP2/asd/statement/Statement.java
@@ -2,10 +2,9 @@ package TP2.asd.statement;
 
 import TP2.SymbolTable;
 import TP2.TypeException;
-import TP2.asd.core.Element;
 import TP2.llvm.Llvm;
 
-public interface Statement extends Element {
+public interface Statement {
     String pp(int nbIndent);
     Llvm.IR toIR(SymbolTable table) throws TypeException, NullPointerException;
 }
