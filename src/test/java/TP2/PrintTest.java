61fc95540ff9ac3aad82a3a874b2f558c7ba7c54
mdugue <mathias.dugue@etudiant.univ-rennes1.fr>
2020-12-19 11:52:45 +0100

    Tests files Read & classes

--- src/test/java/TP2/PrintTest.java
+++ src/test/java/TP2/PrintTest.java
@@ -26,4 +26,63 @@ public class PrintTest {
         Utils.reset();
         return parser.program().out;
     }
+
+    @DisplayName("print d'une String")
+    @Test
+    public void print0() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Print/testPrint0V.vsl");
+        final String result = "";//TODO: résultat en LLVM
+        Program p = createParser(vsl);
+        assertTrue(p.toIR().toString().contains(result));
+    }
+
+    @DisplayName("print d'une variable")
+    @Test
+    public void print1() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Print/testPrint1V.vsl");
+        final String result = "";//TODO: résultat en LLVM
+        Program p = createParser(vsl);
+        assertTrue(p.toIR().toString().contains(result));
+    }
+
+    @DisplayName("print d'une variable non initialisée")
+    @Test
+    public void print2() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Print/testPrint2F.vsl");
+        createParser(vsl);
+        assertEquals(1, parser.getNumberOfSyntaxErrors());
+    }
+
+    @DisplayName("print d'un élément de tableau")
+    @Test
+    public void print3() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Print/testPrint3F.vsl");
+        createParser(vsl);
+        assertEquals(1, parser.getNumberOfSyntaxErrors());
+    }
+
+    @DisplayName("print d'un appel de fonction")
+    @Test
+    public void print4() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Print/testPrint4F.vsl");
+        createParser(vsl);
+        assertEquals(1, parser.getNumberOfSyntaxErrors());
+    }
+
+    @DisplayName("print d'une série de string et variables")
+    @Test
+    public void print5() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Print/testPrint5V.vsl");
+        final String result = "";//TODO: résultat en LLVM
+        Program p = createParser(vsl);
+        assertTrue(p.toIR().toString().contains(result));
+    }
+
+    @DisplayName("print d'une série mais avec un élément de tableau")
+    @Test
+    public void print6() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Print/testPrint6F.vsl");
+        createParser(vsl);
+        assertEquals(1, parser.getNumberOfSyntaxErrors());
+    }
 }
--- src/test/java/TP2/ReadTest.java
+++ src/test/java/TP2/ReadTest.java
@@ -1,5 +1,74 @@
 package TP2;
 
+import TP2.asd.Program;
+import TP2.utils.Utils;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+
+import java.io.IOException;
+
 public class ReadTest {
-    //TODO: tests
+    private VSLParser parser;
+
+    private Program createParser(String input) throws RecognitionException {
+        input = "FUNC INT main() {\n" + input + "\n}";
+        VSLLexer lexer = new VSLLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        parser = new VSLParser(tokens);
+
+        Utils.reset();
+        return parser.program().out;
+    }
+
+    @DisplayName("read d'une variable")
+    @Test
+    public void read0() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Read/testRead0V.vsl");
+        final String result = "";//TODO: résultat en LLVM
+        Program p = createParser(vsl);
+        assertTrue(p.toIR().toString().contains(result));
+    }
+
+    @DisplayName("read d'un élément de tableau")
+    @Test
+    public void read1() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Read/testRead1F.vsl");
+        createParser(vsl);
+        assertEquals(1, parser.getNumberOfSyntaxErrors());
+    }
+
+    @DisplayName("read d'une liste de variables")
+    @Test
+    public void read2() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Read/testRead2V.vsl");
+        final String result = "";//TODO: résultat en LLVM
+        Program p = createParser(vsl);
+        assertTrue(p.toIR().toString().contains(result));
+    }
+
+    @DisplayName("read d'une liste de var mais il y a un élément de tableau")
+    @Test
+    public void read3() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Read/testRead3F.vsl");
+        createParser(vsl);
+        assertEquals(1, parser.getNumberOfSyntaxErrors());
+    }
+
+    @DisplayName("read d'une var non déclarée")
+    @Test
+    public void read4() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Read/testRead4F.vsl");
+        createParser(vsl);
+        assertEquals(1, parser.getNumberOfSyntaxErrors());
+    }
+
+    @DisplayName("read d'une liste de var mais il y a un appel de fonction")
+    @Test
+    public void read5() throws IOException {
+        final String vsl = UtilsFile.getFileContent("testsPersos/Read/testRead5F.vsl");
+        createParser(vsl);
+        assertEquals(1, parser.getNumberOfSyntaxErrors());
+    }
 }
